524ae806f8c538b71f7d535d28adac35824751b4
Administrator <qracle@126.com>
2022-01-10 17:43:56 +0800

    for multiple key vaults

--- README.md
+++ README.md
@@ -9,20 +9,30 @@ export AZURE_ENVIRONMENT=xxxxxx
 export AZURE_CLIENT_ID=xxxxxx
 export AZURE_CLIENT_SECRET=xxxxxx
 export AZURE_TENANT_ID=xxxxxx
-export KVAULT=xxxxxx
 ```
 
 ### Sample
 
 ```
-cli, err := client.NewKeyvaultClient()
-if err != nil {
-    panic(err)
+package main
+
+import (
+	"fmt"
+	"github.com/guobinqiu/keyvault/client"
+)
+
+func main() {
+	vaultBaseURL := "https://cn-apac-kv-np-akstesting.vault.azure.cn/"
+	cli, err := client.NewAzureClient(vaultBaseURL)
+	if err != nil {
+		panic(err)
+	}
+	key := "test-only-pwd"
+	val, err := cli.GetValue(key)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(val)
 }
-key := "test-only-pwd"
-val, err := cli.GetValue(key)
-if err != nil {
-    panic(err)
-}
-fmt.Println(val)
+
 ```
--- client/azure.go
+++ client/azure.go
@@ -7,15 +7,23 @@ import (
 	"github.com/Azure/azure-sdk-for-go/profiles/latest/keyvault/keyvault"
 	kvauth "github.com/Azure/azure-sdk-for-go/services/keyvault/auth"
 	"os"
+	"strings"
 )
 
 type AzureClient struct {
 	keyvaultClient keyvault.BaseClient
+	vaultBaseURL   string
 }
 
-func NewAzureClient() (*AzureClient, error) {
-	if os.Getenv("AZURE_TENANT_ID") == "" || os.Getenv("AZURE_CLIENT_ID") == "" || os.Getenv("AZURE_CLIENT_SECRET") == "" || os.Getenv("KVAULT") == "" {
-		return nil, errors.New("env vars not set, exiting...")
+func NewAzureClient(vaultBaseURL string) (*AzureClient, error) {
+	if isBlank(os.Getenv("AZURE_TENANT_ID")) {
+		return nil, errors.New("missing environment variable AZURE_TENANT_ID")
+	}
+	if isBlank(os.Getenv("AZURE_CLIENT_ID")) {
+		return nil, errors.New("missing environment variable AZURE_CLIENT_ID")
+	}
+	if isBlank(os.Getenv("AZURE_CLIENT_SECRET")) {
+		return nil, errors.New("missing environment variable AZURE_CLIENT_SECRET")
 	}
 
 	authorizer, err := kvauth.NewAuthorizerFromEnvironment()
@@ -26,14 +34,17 @@ func NewAzureClient() (*AzureClient, error) {
 	basicClient := keyvault.New()
 	basicClient.Authorizer = authorizer
 
-	return &AzureClient{basicClient}, nil
+	return &AzureClient{basicClient, vaultBaseURL}, nil
 }
 
 func (cli *AzureClient) GetValue(key string) (string, error) {
-	vaultName := os.Getenv("KVAULT")
-	secretResp, err := cli.keyvaultClient.GetSecret(context.Background(), "https://"+vaultName+".vault.azure.cn", key, "")
+	secretResp, err := cli.keyvaultClient.GetSecret(context.Background(), cli.vaultBaseURL, key, "")
 	if err != nil {
 		return "", fmt.Errorf("unable to get value for secret: %v", err)
 	}
 	return *secretResp.Value, nil
+}
+
+func isBlank(s string) bool {
+	return strings.TrimSpace(s) == ""
 }
